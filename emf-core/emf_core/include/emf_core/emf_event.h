e8fa4254da3b8213ded98e7c9fe07184c424ed0b
Gabriel Borrelli <gabriel.borrelli@nanoshellsoft.com>
2020-10-24 19:18:16 +0200

    Update the documentation of the emf event system

--- emf-core/emf_core/include/emf_core/emf_event.h
+++ emf-core/emf_core/include/emf_core/emf_event.h
@@ -1,3 +1,36 @@
+//! # Introduction
+//! 
+//! The `event` system provides an interface to the creation and management of events.
+//! 
+//! ## Events
+//! 
+//! Events come in two varieties:
+//! 
+//! - Public (named).
+//! - Private (unnamed).
+//! 
+//! ## Example
+//! 
+//! ```c
+//! const char* event_name_str = "example_event";
+//! emf_event_name_t g_event_name;
+//! memset(&g_event_name, 0, sizeof(emf_event_name_t));
+//! 
+//! // Populate the event name.
+//! // Bounds checking is omitted for brevity.
+//! g_event_name.length = strlen(event_name_str);
+//! memcpy(&g_event_name.data, event_name_str, g_event_name.length);
+//! 
+//! emf_lock();
+//! 
+//! emf_event_handle_t g_event = emf_event_create(&g_event_name, NULL);
+//! emf_event_handle_t p_event = emf_event_create_private(&foo);
+//! 
+//! emf_event_subscribe_handler(g_event, &foo);
+//! emf_event_signal(g_event, NULL);
+//! 
+//! emf_unlock();
+//! ```
 #ifndef EMF_INTERFACE_C_EMF_EVENT_H
 #define EMF_INTERFACE_C_EMF_EVENT_H
 
@@ -11,7 +44,8 @@
 #include <emf_core/emf_error_t.h>
 #include <emf_core/emf_macros.h>
 
-#define EMF_EVENT_NAME_MAX_LENGTH 64 /// Max length of an event name.
+/// The maximum length of an event name.
+#define EMF_EVENT_NAME_MAX_LENGTH 64
 
 #ifdef __cplusplus
 namespace EMF::C {
@@ -21,32 +55,55 @@ namespace EMF::C {
 ***********************************************  Types  ***********************************************
 ******************************************************************************************************/
 
-/**
- * Event input.
- */
+/// The input to an event.
 typedef void* emf_event_data_t;
 
-/**
- * A handle to an event.
- */
+/// A handle to an existing event.
+/// 
+/// # Fields
+/// 
+/// - **id**: `int32_t`
+/// 
+///     The id of the event.
 typedef struct emf_event_handle_t {
-    /// Handle id.
+    /// The id of the event.
     int32_t id;
 } emf_event_handle_t;
 
-/**
- * Name of an event.
- */
+/// An event name.
+/// 
+/// A `UTF-8` string describing the name of an event.
+/// The string is not terminated with a `\0` character.
+/// 
+/// # Fields
+/// 
+/// - **data**: [`char[EMF_EVENT_NAME_MAX_LENGTH]`](./constant.EMF_EVENT_NAME_MAX_LENGTH.md)
+/// 
+///     Name string.
+/// 
+/// - **length**: `size_t`
+/// 
+///     Length of the string.
 EMF_FIXED_BUFFER_TYPEDEF(emf_event_name_t, char, EMF_EVENT_NAME_MAX_LENGTH)
 
-/**
- * A span of event names.
- */
+/// A span of event names.
+/// 
+/// # Fields
+/// 
+/// - **data**: [`emf_event_name_t*`](./struct.emf_event_name_t.md)
+/// 
+///     The start of the span.
+/// 
+/// - **length**: `size_t`
+/// 
+///     The length of the span.
 EMF_SPAN_TYPEDEF(emf_event_name_span_t, emf_event_name_t)
 
-/**
- * A callback.
- */
+/// An event handler.
+/// 
+/// The function signature of an event handler accepts an optional 
+/// [`emf_event_data_t`](./type.emf_event_data_t.md) parameter. 
+/// This parameter represents a type erased pointer and is supplied when the event is dispatched.
 EMF_FUNCTION_PTR_T(emf_event_handler, void, emf_event_data_t EMF_MAYBE_NULL data)
 
 #ifdef __cplusplus
@@ -57,191 +114,236 @@ extern "C" {
 *****************************************  System operations  *****************************************
 ******************************************************************************************************/
 
-/**
- * @brief Creates a new public event.
- *
- * Creates a new event with the given name and handler.\n
- *
- * @note Requires synchronisation.
- * @note If <code>event_handler == NULL</code>, then an empty event will be created.
- * @note All names starting with "__" are reserved for the implementors of the "emf::core" module.
- * @note All names starting with "_Interface" are reserved for the implementors of the "Interface" interface.
- *
- * @pre <code>event_name</code> may not be <code>NULL</code>.
- * @pre <code>emf_event_name_exists(event_name) == emf_bool_false</code>.
- *
- * @post A new named event is created.
- *
- * @param event_name Name of the event.
- * @param event_handler Event handler.
- *
- * @return Event handle.
- */
+/// Creates a new public event.
+/// 
+/// Creates a new public event with the name `event_name`. 
+/// An empty event can be created, by passing `NULL` to `event_handler`.
+/// 
+/// # Naming convention
+/// 
+/// Events follow a similar naming convention to the one found in the `C` language:
+/// 
+/// - Names starting with `__` are reserved for the emf-core interface.
+/// - Names starting with `_Interface` (where `Interface` is `I` followed by the name, e.g. `_Iemf::core`) 
+///   are reserved for the corresponding interface.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).  
+/// Furthermore, the caller must ensure that the following preconditions hold:
+/// 
+/// ```c
+/// ASSERT_NE(
+///     event_name,
+///     NULL
+/// );
+/// ASSERT_EQ(
+///     emf_event_name_exists(event_name),
+///     emf_bool_false
+/// );
+/// ```
 emf_event_handle_t EMF_CALL_C emf_event_create(
     const emf_event_name_t* EMF_NOT_NULL event_name, emf_event_handler_fn_t EMF_MAYBE_NULL event_handler) EMF_NOEXCEPT;
 
-/**
- * @brief Creates a new private event
- *
- * Creates a new private event with the given handler.
- *
- * @note Requires synchronisation.
- * @note If <code>event_handler == NULL</code>, then an empty event will be created.
- *
- * @post A new unnamed event is created.
- *
- * @param event_handler Event handler.
- */
+/// Creates a new private event.
+/// 
+/// Private events can only be accessed by the handle. 
+/// An empty event can be created, by passing `NULL` to `event_handler`.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).
 EMF_NODISCARD emf_event_handle_t EMF_CALL_C emf_event_create_private(
     emf_event_handler_fn_t EMF_MAYBE_NULL event_handler) EMF_NOEXCEPT;
 
-/**
- * @brief Destroys an event.
- *
- * @note Requires synchronisation.
- *
- * @pre <code>emf_event_handle_exists(event_handle) == emf_bool_true</code>.
- *
- * @post The event is destroyed.
- * @post <code>event_handle</code> is invalidated.
- *
- * @param event_handle Event handle.
- */
+/// Destroys an event.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).  
+/// Furthermore, the caller must ensure that the following preconditions hold:
+/// 
+/// ```c
+/// ASSERT_EQ(
+///     emf_event_name_exists(event_name),
+///     emf_bool_true
+/// );
+/// ```
 void EMF_CALL_C emf_event_destroy(emf_event_handle_t event_handle) EMF_NOEXCEPT;
 
-/**
- * @brief Makes an event public.
- *
- * Makes a private event visible with a certain name.
- *
- * @note Requires synchronisation.
- * @note A event handle may only have one name.
- * @note All names starting with "__" are reserved for the implementors of the "emf::core" module.
- * @note All names starting with "_Interface" are reserved for the implementors of the "Interface" interface.
- *
- * @pre <code>event_handle</code> has been acquired through emf_event_create_private().
- * @pre <code>event_name</code> may not be <code>NULL</code>.
- * @pre <code>emf_event_name_exists(event_name) == emf_bool_false</code>.
- *
- * @post The event is assigned a name.
- *
- * @param event_handle Handle.
- * @param event_name New name.
- */
+/// Converts a private event into a public event.
+/// 
+/// The conversion is applied, by assigning a name to the private event.
+/// See [`emf_event_create()`](./fn.emf_event_create.md) for more information.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).  
+/// Furthermore, the caller must ensure that the following preconditions hold:
+/// 
+/// ```c
+/// ASSERT_EQ(
+///     emf_event_handle_exists(event_handle),
+///     emf_bool_true
+/// );
+/// ASSERT_NE(
+///     event_name,
+///     NULL
+/// );
+/// ASSERT_EQ(
+///     emf_event_name_exists(event_name),
+///     emf_bool_false
+/// )
+/// ```
 void EMF_CALL_C emf_event_publish(emf_event_handle_t event_handle, const emf_event_name_t* EMF_NOT_NULL event_name) EMF_NOEXCEPT;
 
-/**
- * @brief Get the number of public events.
- *
- * @note Requires synchronisation.
- *
- * @return Number of public events.
- */
+/// Returns the number of public events.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).
 EMF_NODISCARD size_t EMF_CALL_C emf_event_get_num_public_events() EMF_NOEXCEPT;
 
-/**
- * @brief Copies all public events into a buffer.
- *
- * @note Requires synchronisation.
- *
- * @pre <code>buffer</code> may not be <code>NULL</code>.
- * @pre <code>buffer.length >= emf_event_get_num_public_events()</code>.
- * @pre <code>buffer.data != NULL</code>.
- *
- * @post <code>buffer</code> contains the event names.
- *
- * @param buffer Destination.
- *
- * @return Number of public events.
- */
+/// Copies all public events into a buffer.
+/// 
+/// The names of all public events are copied into `buffer`.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).  
+/// Furthermore, the caller must ensure that the following preconditions hold:
+/// 
+/// ```c
+/// ASSERT_NE(
+///     buffer,
+///     NULL
+/// );
+/// ASSERT_NE(
+///     buffer->data,
+///     NULL
+/// );
+/// ASSERT(
+///     buffer->length >= emf_event_get_num_public_events()
+/// );
+/// ```
 size_t EMF_CALL_C emf_event_get_public_events(emf_event_name_span_t* EMF_NOT_NULL buffer) EMF_NOEXCEPT;
 
-/**
- * @brief Get the event associated with a specific name.
- *
- * @note Requires synchronisation.
- *
- * @pre <code>event_name</code> may not be <code>NULL</code>.
- * @pre <code>emf_event_name_exists(event_name) == emf_bool_true</code>.
- *
- * @param event_name Name.
- *
- * @return Event handle
- */
+/// Fetches the handle of a public event.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).  
+/// Furthermore, the caller must ensure that the following preconditions hold:
+/// 
+/// ```c
+/// ASSERT_NE(
+///     event_name,
+///     NULL
+/// );
+/// ASSERT_EQ(
+///     emf_event_name_exists(event_name),
+///     emf_bool_true
+/// );
+/// ```
 EMF_NODISCARD emf_event_handle_t EMF_CALL_C emf_event_get_event_handle(
     const emf_event_name_t* EMF_NOT_NULL event_name) EMF_NOEXCEPT;
 
-/**
- * @brief Check if a handle exists.
- *
- * @note Requires synchronisation.
- *
- * @param event_handle Handle.
- *
- * @return <code>emf_bool_true</code> if the handle exists.
- * @return <code>emf_bool_false</code> if the handle does not exist.
- */
+/// Checks for the existence of a handle.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).
 EMF_NODISCARD emf_bool_t EMF_CALL_C emf_event_handle_exists(emf_event_handle_t event_handle) EMF_NOEXCEPT;
 
-/**
- * @brief Check if the named event exists.
- *
- * @note Requires synchronisation.
- *
- * @pre <code>event_name</code> may not be <code>NULL</code>
- *
- * @param event_name Name.
- *
- * @return <code>emf_bool_true</code> if the name exists.
- * @return <code>emf_bool_false</code> if the name does not exist or <code>event_name == NULL</code>.
- */
+/// Checks for the existence of a name.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).  
+/// Furthermore, the caller must ensure that the following preconditions hold:
+/// 
+/// ```c
+/// ASSERT_NE(
+///     event_name,
+///     NULL
+/// );
+/// ```
 EMF_NODISCARD emf_bool_t EMF_CALL_C emf_event_name_exists(const emf_event_name_t* EMF_NOT_NULL event_name) EMF_NOEXCEPT;
 
 /******************************************************************************************************
 *****************************************  Event operations  *****************************************
 ******************************************************************************************************/
 
-/**
- * @brief Subscribe to an event.
- *
- * @note Requires synchronisation.
- * @note Appends the event_handler at the end of the call list.
- *
- * @pre <code>emf_event_handle_exists(event_handle) == emf_bool_true</code>.
- * @pre <code>event_handler</code> may not be <code>NULL</code>
- * @pre <code>event_handler</code> may not be already subscribed.
- *
- * @param event_handle Event handle.
- * @param event_handler Handler.
- */
+/// Subscribes to an event.
+/// 
+/// The `event_handler` is added to the end of the call list.
+/// 
+/// # Event input
+/// 
+/// Not every `event_handler` is compatible with an event. 
+/// The creator of an event must define how the input will be interpreted. 
+/// Incorrect usage of an event may lead errors that are difficult to find.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).  
+/// A event handler may not subscribe twice to the same event.  
+/// Furthermore, the caller must ensure that the following preconditions hold:
+/// 
+/// ```c
+/// ASSERT_EQ(
+///     emf_event_handle_exists(event_handle),
+///     emf_bool_true
+/// );
+/// ASSERT_NE(
+///     event_handler,
+///     NULL
+/// );
+/// ```
 void EMF_CALL_C emf_event_subscribe_handler(
     emf_event_handle_t event_handle, emf_event_handler_fn_t EMF_NOT_NULL event_handler) EMF_NOEXCEPT;
 
-/**
- * @brief Unsubscribe from an event.
- *
- * @note Requires synchronisation.
- *
- * @pre <code>emf_event_handle_exists(event_handle) == emf_bool_true</code>.
- * @pre <code>event_handler</code> may not be <code>NULL</code>
- * @pre <code>event_handler</code> must be subscribed.
- *
- * @param event_handle Event handle.
- * @param event_handler Handler.
- */
+/// Unsubscribes from an event.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).  
+/// The event handler must already be subscribed.  
+/// Furthermore, the caller must ensure that the following preconditions hold:
+/// 
+/// ```c
+/// ASSERT_EQ(
+///     emf_event_handle_exists(event_handle),
+///     emf_bool_true
+/// );
+/// ASSERT_NE(
+///     event_handler,
+///     NULL
+/// );
+/// ```
 void EMF_CALL_C emf_event_unsubscribe_handler(
     emf_event_handle_t event_handle, emf_event_handler_fn_t EMF_NOT_NULL event_handler) EMF_NOEXCEPT;
 
-/**
- * @brief Fire an event
- *
- * @note Requires synchronisation.
- *
- * @pre <code>emf_event_handle_exists(event_handle) == emf_bool_true</code>.
- *
- * @param event_handle Event handle.
- * @param event_data Data.
- */
+/// Fires an event.
+/// 
+/// # Event input
+/// 
+/// The parameter `event_data` is passed to every handler in the event. 
+/// The creator of an event should define the structure, mutability and valid operations of the input.
+/// 
+/// See [`emf_event_subscribe_handler()`](./fn.emf_event_subscribe_handler.md) for more information.
+/// 
+/// # Undefined Behaviour
+/// 
+/// The callee expects that the caller holds a lock (See [emf_lock()](./fn.emf_lock.md)).  
+/// Furthermore, the caller must ensure that the following preconditions hold:
+/// 
+/// ```c
+/// ASSERT_EQ(
+///     emf_event_handle_exists(event_handle),
+///     emf_bool_true
+/// );
+/// ```
 void EMF_CALL_C emf_event_signal(emf_event_handle_t event_handle, emf_event_data_t EMF_MAYBE_NULL event_data) EMF_NOEXCEPT;
 
 #ifdef __cplusplus
