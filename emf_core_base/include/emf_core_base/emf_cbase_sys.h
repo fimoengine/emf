0b7bc8948cca098517545f0c1fa11a706d0438d8
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2021-05-23 11:19:15 +0200

    Fix wrong function signature

--- emf_core_base/include/emf_core_base/emf_cbase_sys.h
+++ emf_core_base/include/emf_core_base/emf_cbase_sys.h
@@ -20,6 +20,7 @@ typedef struct emf_cbase_t emf_cbase_t;
 
 EMF_CBASE_FUNCTION_PTR_T(emf_cbase_fn_t, void, void)
 EMF_CBASE_OPTIONAL_TYPEDEF(emf_cbase_fn_optional_t, emf_cbase_fn_t EMF_CBASE_NOT_NULL)
+EMF_CBASE_OPTIONAL_TYPEDEF(emf_cbase_sync_handler_interface_optional_t, emf_cbase_sync_handler_interface_t)
 
 // sync handler interface
 typedef struct emf_cbase_sync_handler_t emf_cbase_sync_handler_t;
@@ -59,7 +60,7 @@ EMF_CBASE_FUNCTION_PTR_T(emf_cbase_sys_unlock_fn_t, void, emf_cbase_t* EMF_CBASE
 EMF_CBASE_FUNCTION_PTR_T(emf_cbase_sys_get_sync_handler_fn_t, EMF_CBASE_NODISCARD emf_cbase_sync_handler_interface_t,
     emf_cbase_t* EMF_CBASE_MAYBE_NULL base_module)
 EMF_CBASE_FUNCTION_PTR_T(emf_cbase_sys_set_sync_handler_fn_t, void, emf_cbase_t* EMF_CBASE_MAYBE_NULL base_module,
-    emf_cbase_sync_handler_interface_t sync_handler)
+    emf_cbase_sync_handler_interface_optional_t sync_handler)
 
 #ifdef __cplusplus
 }
